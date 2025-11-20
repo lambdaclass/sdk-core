2ea5c76a8b3904a65eea5a33655666b08e71780e
Manuel Iñaki Bilbao <manuel.bilbao@lambdaclass.com>
2025-11-19 23:57:18 -0300

    Update WETH

--- src/entities/weth9.ts
+++ src/entities/weth9.ts
@@ -23,5 +23,5 @@ export const WETH9: { [chainId: number]: Token } = {
   [56]: new Token(56, '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c', 18, 'WBNB', 'Wrapped BNB'),
   [137]: new Token(137, '0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270', 18, 'WMATIC', 'Wrapped MATIC'),
   [43114]: new Token(43114, '0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7', 18, 'WAVAX', 'Wrapped AVAX'),
-  [65536999]: new Token(65536999, '0x36ccfc7163a2c2cdf7a6d6da202eb9c7aa18e4ea', 18, 'ETH', 'Wrapped Ether')
+  [65536999]: new Token(65536999, '0x000000000000000000000000000000000000FfFD', 18, 'ETH', 'Wrapped Ether')
 }
